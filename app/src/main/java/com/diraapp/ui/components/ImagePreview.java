164258a23a848e3acd4a501d9ac6cf5e37a9c781
polbater <51130679+karlovm@users.noreply.github.com>
2023-10-24 22:06:26 +0300

    Fix blinking

--- app/src/main/java/com/diraapp/ui/components/ImagePreview.java
+++ app/src/main/java/com/diraapp/ui/components/ImagePreview.java
@@ -121,14 +121,15 @@ public class ImagePreview extends RelativeLayout {
         isMainImageLoaded = false;
         this.attachment = attachment;
         overlay.setVisibility(GONE);
-        imageView.setImageBitmap(null);
+        imageView.setImageBitmap(Bitmap.createBitmap(attachment.getWidth(),
+                attachment.getHeight(),
+                Bitmap.Config.ARGB_8888));
         DiraActivity.runGlobalBackground(() -> {
             Bitmap previewBitmap = attachment.getBitmapPreview();
             if (previewBitmap == null) {
                 previewBitmap = Bitmap.createBitmap(attachment.getWidth(),
                         attachment.getHeight(),
                         Bitmap.Config.ARGB_8888);
-
             }
             Bitmap finalPreviewBitmap = previewBitmap;
             new Handler(Looper.getMainLooper()).post(() -> {
